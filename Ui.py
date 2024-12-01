6ad4ec25370fa504ffdb3b03296775ffb82abc4d
admin <polyanskiy.yelisey@mail.ru>
2024-12-01 10:22:56 +0500

    Расставил пробелы для удобства

--- Ui.py
+++ Ui.py
@@ -5,11 +5,14 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(600, 600)
+
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23))
         self.pushButton.setObjectName("pushButton")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 21))
